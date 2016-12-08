8e4be600c3370476b038cc0ba72c3e29253f42fb
chrislample <clample@testobject.com>
2016-12-08 15:43:20 +0100

    Integrate Appium with the tests

--- AppiumBasicSetup/BasicTest.cs
+++ AppiumBasicSetup/BasicTest.cs
@@ -1,14 +1,26 @@
 ﻿using NUnit.Framework;
 using System;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Appium;
+
 namespace AppiumBasicSetup
 {
 	[TestFixture()]
 	public class BasicTest
 	{
+		AndroidDriver<AppiumWebElement> driver;
+
 		[SetUp()]
 		public void SetUp()
 		{
 			Console.Write("Setting up the test");
+			DesiredCapabilities capabilities = new DesiredCapabilities();
+			capabilities.SetCapability("testobject_api_key", "YOUR API KEY");
+			capabilities.SetCapability("testobject_device", "Samsung_Galaxy_S6_real");
+			Uri server = new Uri("http://appium.testobject.com/wd/hub");
+
+			driver = new AndroidDriver<AppiumWebElement>(server, capabilities);
 		}
 
 		[Test()]
@@ -27,6 +39,7 @@ namespace AppiumBasicSetup
 		[TearDown()]
 		public void TearDown()
 		{
+			driver.Quit();
 			Console.Write("Tearing down the test");
 		}
 	}
