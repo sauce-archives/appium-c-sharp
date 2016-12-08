a2d26b37919a52351e8ae7e3de7c60ee8e34fd14
chrislample <clample@testobject.com>
2016-12-08 14:41:48 +0100

    Run basic test setup printing to the console

---
+++ AppiumBasicSetup/BasicTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+namespace AppiumBasicSetup
+{
+	[TestFixture()]
+	public class BasicTest
+	{
+		[SetUp()]
+		public void SetUp()
+		{
+			Console.Write("Setting up the test");
+		}
+
+		[Test()]
+		public void FirstTest()
+		{
+			Console.Write("Running the first test");
+		}
+			       
+
+		[Test()]
+		public void TestCase()
+		{
+			Console.Write("Running the second test");
+		}
+
+		[TearDown()]
+		public void TearDown()
+		{
+			Console.Write("Tearing down the test");
+		}
+	}
+}
